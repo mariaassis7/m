becc4e29a97217b38a168b3902418c963e206365
mariaassis7 <mariazkx888@gmail.com>
2024-08-07 11:44:11 -0300

    adicionado js e css

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(600, 600);
+    background ("black")
+  }
+  
+  function draw() {
+    
+    stroke("blue");
+    fill("yellow");
+    
+    // console.log(mouseIsPressed) 
+    
+    if(mouseIsPressed) {
+      rect(mouseX, mouseY, 35, 50);
+    }
+  }
